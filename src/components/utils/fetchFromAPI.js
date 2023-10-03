6a48e58e0b65ab064adea7ac444271291480207d
maheepatel <mahendarptl9@gmail.com>
2023-10-03 09:53:46 +0530

    ignore the file

--- src/components/utils/fetchFromAPI.js
+++ src/components/utils/fetchFromAPI.js
@@ -7,9 +7,8 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    // process.env.REACT_APP_RAPID_API_KEY
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    // "X-RapidAPI-Key": "ee0fd1c787msh414cf1a58a2c538p10d5e6jsna13e70e0a9e5",
+
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
